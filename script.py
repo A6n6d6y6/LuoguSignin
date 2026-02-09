8dcd0e000ba289a0417a5947742ce07774df77da
A6n6d6y6 <157708429+A6n6d6y6@users.noreply.github.com>
2026-02-09 12:04:34 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,9 @@
+# coding=utf-8
+
 import requests,json,sys,io
 
 def punch(cookie):
-    resp = requests.get('https://www.luogu.com.cn/index/ajax_punch',headers={
+    return requests.get('https://www.luogu.com.cn/index/ajax_punch',headers={
         'accept':'*/*',
         'accept-encoding':'gzip,deflate,br',
         'accept-language':'zh-CN,zh;q=0.9',
@@ -12,8 +14,7 @@ def punch(cookie):
         'user-agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.62 Safari/537.36',
         'x-csrf-token':'1530786066:F3dU7fTlJSTWKNA9XMdI76gdJGRdkxQqxafCS4PgGw8=',
         'x-requested-with':'XMLHttpRequest'
-    })
-    return resp.content.decode("gbk")
+    }).text
 
 if __name__=="__main__":
     for i in range(1,len(sys.argv)):
