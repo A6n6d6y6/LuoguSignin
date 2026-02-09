25363bf6b2ae3a0756b61378f8e83b964c85cfbf
A6n6d6y6 <157708429+A6n6d6y6@users.noreply.github.com>
2026-02-09 12:26:18 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,11 @@
 # coding=utf-8
 
-import requests,json,sys,io
-
-sys.stdout=io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-sys.stderr=io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+import requests
+import json
+import sys
 
 def punch(cookie):
-	res=requests.get('https://www.luogu.com.cn/index/ajax_punch',headers={
+    return requests.get('https://www.luogu.com.cn/index/ajax_punch', headers={
 		'accept':'*/*',
 		'accept-encoding':'gzip,deflate,br',
 		'accept-language':'zh-CN,zh;q=0.9',
@@ -17,20 +16,22 @@ def punch(cookie):
 		'user-agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.62 Safari/537.36',
 		'x-csrf-token':'1530786066:F3dU7fTlJSTWKNA9XMdI76gdJGRdkxQqxafCS4PgGw8=',
 		'x-requested-with':'XMLHttpRequest'
-	})
-	res.encoding=res.apparent_encoding
-	return res.text
+	}).text
 
-if __name__=="__main__":
-	for i in range(1,len(sys.argv)):
-		print(f"Cookie:{sys.argv[i]}")
-		response=punch(sys.argv[i])
-		print(response)
-		try:
-			tmp=json.loads(response)
-			if tmp['code']==200:
-				print('code=',tmp['code'],'message =',tmp['more']['html'])
-			else:
-				print('code=',tmp['code'],'message =',tmp['message'])
-		except Exception as err:
-			print(f"<{err}>")
+if __name__ == "__main__":
+    print(f"Script Name: {sys.argv[0]}")
+    for i in range(1, len(sys.argv)):
+        if sys.argv[i][-1] != '*':
+            print(f"Cookie: {sys.argv[i][:-10]}zkyakioi")
+            print(f"sjyakioi{sys.argv[i][-10:]}")
+        response = punch(sys.argv[i])
+        print(f"No. {i}:")
+        try:
+            print(response)
+            tmp = json.loads(response)
+            if tmp['code'] == 200:
+                print('code =', tmp['code'], 'message =', tmp['more']['html'])
+            else:
+                print('code =', tmp['code'], 'message =', tmp['message'])
+        except Exception as err:
+            print(f"<{err}>")
